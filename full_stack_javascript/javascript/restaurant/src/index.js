cb901c620dcc33dc2db19584e63f3e21284c5a8c
Eduardo Fernandes de Magalhaes Pimenta <ed83isme@gmail.com>
2024-08-08 21:09:38 +0000

    added basic structure to the restaurant page using webpack

---
+++ full_stack_javascript/javascript/restaurant/src/index.js
@@ -0,0 +1,4 @@
+import header from "./js/header.js";
+
+console.log("imported Header succesfully", header());
+document.querySelector("#content").appendChild(header());
---
+++ full_stack_javascript/javascript/restaurant/src/js/header.js
@@ -0,0 +1,32 @@
+import BakeryShelves from "../img/bakery shelves.jpeg";
+
+function header() {
+  let headerMain = document.createElement("div");
+  let headerTitleWraper = document.createElement("div");
+  let headerImgWraper = document.createElement("div");
+  let headerTitle = document.createElement("h1");
+  let headerImg = new Image();
+  let headerCaption = document.createElement("h2");
+  let headerButton = document.createElement("button");
+
+  headerTitle.textContent = "Tudo Trigo";
+  headerCaption.textContent =
+    "Bom vindo a nossa aconchegante padaria. Venha degustar os nossos deliciosos pães artesanais.";
+  headerButton.textContent = "Serviços";
+
+  // Add the image to our existing div.
+  headerImg.src = BakeryShelves; //
+
+  headerImgWraper.appendChild(headerImg);
+  headerTitleWraper.appendChild(headerTitle);
+  headerTitleWraper.appendChild(headerCaption);
+  headerTitleWraper.appendChild(headerButton);
+
+  headerMain.appendChild(headerImgWraper);
+  headerMain.appendChild(headerTitleWraper);
+
+  console.log(headerMain);
+  return headerMain;
+}
+
+export default header;
